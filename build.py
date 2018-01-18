fd18cf96de08d66f45dc23e3848f9e5e411c0910
JamesL9 <jamieleaper@yahoo.co.uk>
2018-01-18 02:12:04 +0000

    #cool!

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 from gameplay import *
 #cool!
+#1231
 # this was raw code without any architecture now we need to think about an actual structure for
 # our application
 # lets think about a "build" , something you choose with 3 things : a champion , a set of runes
