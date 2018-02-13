9470533595ba8b832bd582d6d54ff6a837867841
Alexandre Du lorier <alexandre.dulorier@epitech.eu>
2018-02-14 00:08:28 +0100

    osef

--- app2.py
+++ app2.py
@@ -8,41 +8,48 @@ builder = build.Build()
 class ChampionList(wx.ListCtrl):
     def __init__(self, parent, id):
         wx.ListCtrl.__init__(self, parent, id,
-                             style=wx.LC_ICON
-                                   | wx.BORDER_NONE
-                                   | wx.LC_NO_HEADER
+                             style=wx.LC_ICON | wx.BORDER_NONE | wx.LC_NO_HEADER,
+                             pos=(0, 100),
+                             size=(800, 400)
                              )
         BMP_SIZE = 120
         self.il = wx.ImageList(BMP_SIZE, BMP_SIZE)
 
         self.imglistdict = {}
-        for index, champ in enumerate(builder.champions.keys()):
-            file = 'data/champion_squares/' + champ + '.png'
+        for key, value in builder.champions.items():
+            file = 'data/champion_squares/' + key + '.png'
             if not os.path.isfile(file):
                 continue
             image = wx.Image(file, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-            self.imglistdict[index] = self.il.Add(image)
-
+            self.imglistdict[value['name'].lower()] = self.il.Add(image)
         self.SetImageList(self.il, wx.IMAGE_LIST_NORMAL)
 
-        self.PopulateList()
+        self.populate_list(all=True)
 
-    def PopulateList(self):
-        for key, value in self.imglistdict.items():
-            index = self.InsertItem(key, value)
-            #self.SetItemData(index, key)
+    def populate_list(self, name=None, all=False):  # awkward ambivalent function called to populate the list
+        if name:
+            name = name.lower()
+        self.ClearAll()
+        for index, (key, value) in enumerate(self.imglistdict.items()):  # key is the name of champ , value its graphical representation and index just the place it will get
+            if all or key.startswith(name):  # if ahri starts with 'a'
+                self.InsertItem(index, value)
 
 
 class ChampionFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, -1, title, size=(800, 600))
 
-        self.InitUI()
+        self.__init_ui()
         self.Centre()
         self.Show()
 
-    def InitUI(self):
-        ChampionList(self, -1)
+    def change_list(self, event):
+        self.list.populate_list(self.text.GetLineText(0))  # call list function and sending it string input of the widget
+
+    def __init_ui(self):
+        self.text = wx.TextCtrl(self)  # makes input line for selection
+        self.Bind(wx.EVT_TEXT, self.change_list)  # binds any text input to change list func
+        self.list = ChampionList(self, -1)  # creates the actual list
 
 
 class MDIFrame(wx.MDIParentFrame):
